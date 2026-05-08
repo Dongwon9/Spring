2b823daf505e3907506f99c6d936fda95cdae236
Dongwon9 <goodongwon329@gmail.com>
2026-05-08 17:53:27 +0900

    싱글톤 빈과 함께 사용시 provider로 문제 해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -20,28 +21,25 @@ public class SingletonWithPrototypeTest1 {
         prototypeBean2.addCount();
         assertThat(prototypeBean2.getCount()).isEqualTo(1);
     }
-    
-    @Test
-    void singletonClientUsePrototype() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
-        ClientBean clientBean1 = ac.getBean(ClientBean.class);
-        int count1 = clientBean1.logic();
-        assertThat(count1).isEqualTo(1);
-        ClientBean clientBean2 = ac.getBean(ClientBean.class);
-        int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
-    }
+//    @Test
+//    void singletonClientUsePrototype() {
+//        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+//        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+//        int count1 = clientBean1.logic();
+//        assertThat(count1).isEqualTo(1);
+//        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+//        int count2 = clientBean2.logic();
+//        assertThat(count2).isEqualTo(2);
+//    }
     
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성시점에 주
-        
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
         
+        //ObjectProvider로도 가능.
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
