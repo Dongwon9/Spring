d7d87eed28c8ed7b2c334176c77d268a431fef41
Dongwon9 <goodongwon329@gmail.com>
2026-06-02 17:28:52 +0900

    View 분리 - v2

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,19 @@
+package hello.servlet.web.frontcontroller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyView {
+    private final String viewPath;
+    
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+    
+    public void render(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher(viewPath).forward(req, resp);
+    }
+}
